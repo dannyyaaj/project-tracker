e90ab465ec152ba6ae2261220841ffe6d079e154
Danny Yang <dannyyaaj@gmail.com>
2018-07-19 17:24:55 -0500

    set up client side routes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,8 +1,19 @@
 const app = angular.module('ProjectTrackerApp', ['ngRoute']);
 
-app.config(function($routeProvider) {
-  $routeProvider.when('/', {
-    templateUrl: '../views/entry.html',
-    controller: 'EntryController as vm'
-  })
+app.config(function ($routeProvider) {
+
+  $routeProvider
+    .when('/', {
+      templateUrl: '../views/entry.html',
+      controller: 'EntryController as vm'
+    })
+    .when('/project', {
+      templateUrl: '../views/project.html',
+      controller: 'ProjectController as vm'
+    })
+    .when('/report', {
+      templateUrl: '../views/report.html',
+      controller: 'ReportController as vm'
+    })
+    .otherwise({template: '<h1>404</h1>'})
 })
