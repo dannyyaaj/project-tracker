f8e40c7f75b9b566a5164d6dc290c04246c37a3f
Danny Yang <dannyyaaj@gmail.com>
2018-07-19 11:24:43 -0500

    updated table creation and added dummy data

--- project-tracker.sql
+++ project-tracker.sql
@@ -1,4 +1,38 @@
 -- Create Database
 CREATE DATABASE "project_tracker";
 
--- Create tables
+-- Make tables
+
+-- Project Table
+CREATE TABLE "projects"
+(
+  "id" SERIAL PRIMARY KEY,
+  "project_name" VARCHAR(100),
+  "completed" BOOLEAN DEFAULT False
+);
+
+-- Entries Table
+CREATE TABLE "entries"
+(
+  "id" SERIAL PRIMARY KEY,
+  "project_id" INT REFERENCES "projects",
+  "description" VARCHAR(100),
+  "date" DATE NOT NULL DEFAULT CURRENT_DATE,
+  "start_time" TIME NOT NULL DEFAULT NOW(),
+  "end_time" TIME NOT NULL
+);
+
+-- Dummy Data
+
+-- projects
+INSERT INTO "projects"
+  ("project_name")
+VALUES
+  ('Koala Holla');
+
+-- entries
+INSERT INTO "entries"
+  ("project_id","description","date","start_time", "end_time")
+VALUES
+  (1, 'set up static files', CURRENT_DATE, NOW(), NOW() + interval
+'2 hours');
