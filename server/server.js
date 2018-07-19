50669eac94a413ff0389024cfec71894c888d698
Danny Yang <dannyyaaj@gmail.com>
2018-07-19 14:06:59 -0500

    set up server side routes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const pool = require('./modules/pool');
 const bodyParser = require('body-parser');
 const entryRouter = require('./routes/entry.router');
 const projectRouter = require('./routes/project.router');
