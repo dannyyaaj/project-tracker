a07798aea63962f45cc15f6a6360920ae928bbd9
Danny Yang <dannyyaaj@gmail.com>
2018-07-20 19:22:55 -0500

    set up get request for projects

--- server/routes/project.router.js
+++ server/routes/project.router.js
@@ -2,5 +2,17 @@ const express = require('express');
 let router = express.Router();
 let pool = require('../modules/pool');
 
+router.get('/', (req, res) => {
+  console.log('get projects');
+  const queryText = `SELECT "projects".id, "projects".project_name, "projects".project_owner FROM "projects";`;
+  pool.query(queryText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log('Error selecting entries');
+      res.sendStatus(500);
+    })
+})
 
 module.exports = router;
